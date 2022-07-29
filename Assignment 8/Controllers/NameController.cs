ab61496ed6c18a2cf58cf3452744f786bcbda308
KaleifC <covingka@mail.uc.edu>
2022-07-28 20:15:11 -0400

    inst done

---
+++ Assignment 8/Controllers/NameController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Assignment_8.Models;
+
+namespace Assignment_8.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NameController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var personName = new Name { PersonName = "Kaleif Covington"};
+            return Ok(personName);
+        }
+    }
+}
---
+++ Assignment 8/Models/Name.cs
@@ -0,0 +1,7 @@
+﻿namespace Assignment_8.Models
+{
+    public class Name
+    {
+        public string PersonName { get; set; }
+    }
+}
